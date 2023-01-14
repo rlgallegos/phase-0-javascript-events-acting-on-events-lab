ad70ab5382c87e3dddb01b7985d491b2a673252e
Robert Gallegos <rlgallegos85@gmail.com>
2023-01-13 23:39:46 -0500

    acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+
+//create variable for dodger element
+const dodger = document.getElementById('dodger');
+//change color
+dodger.style.backgroundColor = '#FF69B4';
+//move UP
+dodger.style.bottom = '0px';
+
+
+//add event listener/move conditions
+//LEFT MOVEMENT
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    }
+});
+//RIGHT MOVEMENT
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+});
+
+
+function moveDodgerLeft() {
+    //prepare the number to be worked with (remove pixels qualifier and make it an int)
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+
+    //conditionally "move" to the left (decrease margin)
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }  
+}
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace('px', '');
+    const right = parseInt(rightNumbers, 10);
+
+    if (right < 360) {
+        dodger.style.left = `${right + 1}px`;
+    }
+}
